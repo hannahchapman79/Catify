754d35eb730ac4c918087193133f9fc6e4d31d5c
BassManLuke <luke_haz@yahoo.co.uk>
2024-07-30 15:59:31 +0100

    add styling

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -5,7 +5,7 @@ import Carousel from 'react-bootstrap/Carousel';
 const CatCard = ({catUrl, catId}) => {
     return (
         <li key={catId} className="cat-card">
-            <Expand>
+            <Expand className="button-and-cat">
                 <img key={catUrl} src={catUrl}/>
             </Expand>
         </li>
