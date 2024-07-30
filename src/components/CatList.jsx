e66612a2650d872c4729834d50120d8432b59999
hannahchapman79 <hannahchapman1999@aol.com>
2024-07-30 11:36:03 +0100

    updates repo to fetch cats from an api using axios, with a loading animation

--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react"
 import CatCard from "./CatCard"
+import axios from "axios"
+import loadingAnimation from "./Loading.json"
+import Lottie from "lottie-react"
 
-
-
-
+const apiKey = "live_bQMl8MCnVXxltWqn0ROrEbGf9OeVMPbWPPC0gyqaRGpROGsM9OzOtgkVMzlADgpT";
 
 const CatList = ({searchTerm}) => {
-
-
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=${searchTerm}`).then( (response) => {
-       return response.json();
-    }).then((body) => {
-        console.log(body);
-    })
-
-
+    const [cats, setCats] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
     
-    const [cats, setCats] = useState(searchTerm)
+    
+    useEffect(() => {
+        setIsLoading(true)
+        axios.get(`https://api.thecatapi.com/v1/images/search?api_key=${apiKey}&limit=${searchTerm}`).then((response) => {
+            setCats(response.data)
+            setIsLoading(false)
+        })
+    }, [searchTerm])
+    
+    if (isLoading) {
+        return <Lottie className="loading-animation" animationData={loadingAnimation} loop="true"/>
+    }
+
     return (
         <ol className="cat-list">
             {cats.map((cat) => {
--- src/components/CatProvider.jsx
+++ src/components/CatProvider.jsx
@@ -2,74 +2,12 @@ import { useState } from "react"
 import CatList from "./CatList"
 import CatSearch from "./CatSearch"
 
-const catImages = [
-    {
-      "id": "6i1",
-      "url": "https://cdn2.thecatapi.com/images/6i1.jpg",
-      "width": 494,
-      "height": 500
-    },
-    {
-      "id": "9fk",
-      "url": "https://cdn2.thecatapi.com/images/9fk.jpg",
-      "width": 400,
-      "height": 306
-    },
-    {
-      "id": "aph",
-      "url": "https://cdn2.thecatapi.com/images/aph.jpg",
-      "width": 720,
-      "height": 960
-    },
-    {
-      "id": "c2b",
-      "url": "https://cdn2.thecatapi.com/images/c2b.jpg",
-      "width": 1200,
-      "height": 900
-    },
-    {
-      "id": "cpk",
-      "url": "https://cdn2.thecatapi.com/images/cpk.jpg",
-      "width": 1280,
-      "height": 765
-    },
-    {
-      "id": "d7s",
-      "url": "https://cdn2.thecatapi.com/images/d7s.jpg",
-      "width": 324,
-      "height": 394
-    },
-    {
-      "id": "dj5",
-      "url": "https://cdn2.thecatapi.com/images/dj5.jpg",
-      "width": 720,
-      "height": 720
-    },
-    {
-      "id": "MjA4MzQ4NQ",
-      "url": "https://cdn2.thecatapi.com/images/MjA4MzQ4NQ.jpg",
-      "width": 500,
-      "height": 325
-    },
-    {
-      "id": "yZE2JpeXz",
-      "url": "https://cdn2.thecatapi.com/images/yZE2JpeXz.jpg",
-      "width": 1280,
-      "height": 720
-    },
-    {
-      "id": "d8AjzQxLp",
-      "url": "https://cdn2.thecatapi.com/images/d8AjzQxLp.jpg",
-      "width": 1600,
-      "height": 1041
-    }]
-
 const CatProvider = () => {
-    const [searchTerm, setSearchTerm] = useState(catImages)
+    const [searchTerm, setSearchTerm] = useState(1)
     return (
         <>
         <CatSearch setSearchTerm={setSearchTerm}/>
-            <CatList searchTerm={searchTerm}/>
+            <CatList searchTerm={searchTerm} className="cat-list"/>
         </>
     )
 }
